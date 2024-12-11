1a62c8b438f3fd403a801dcb9b105fca4f47e82b
Nathan Bo <nathan.github22@gmail.com>
2024-12-11 16:17:06 +0100

    peut ajouter un produit a son panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\ContenuPanier;
+use App\Entity\Panier;
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +15,57 @@ class PanierController extends AbstractController
     #[Route('/panier', name: 'app_panier')]
     public function index(): Response
     {
+
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $message = "";
+        $panier = $user->getPanier();
+        $contenuPanier = $panier->getContenuPanier();
+
+        if (!$panier) {
+            $message = "Vous n'avez pas de panier";
+        } elseif (!$contenuPanier) {
+            $message = "Votre panier est vide";
+        }
+
         return $this->render('panier/index.html.twig', [
-            'controller_name' => 'PanierController',
+            "message" => $message,
+            "contenuPanier" => $contenuPanier,
         ]);
+    }
+
+    #[Route("/panier/ajouter/{id}", name: "app_add_panier")]
+    function add(EntityManagerInterface $entityManager, $id)
+    {
+
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $produit = $entityManager->getRepository(Produit::class)->find($id);
+
+        if (!$produit) {
+            throw $this->createNotFoundException("Produit non trouvé.");
+        }
+
+        $panier = $user->getPanier();
+
+        // Création et ajout d'un nouvel objet ContenuPanier
+        $contenuPanier = new ContenuPanier();
+        $contenuPanier->addProduit($produit);
+        $contenuPanier->setQuantite(1);
+        $contenuPanier->setDate(new \DateTime());
+        $contenuPanier->setPanier($panier);
+
+        $entityManager->persist($contenuPanier);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_panier');
     }
 }
